89fb961c4e977172d158f5dc62e29044bd31feb9
santiago alejandro salinas vargas <s.salinasv@uniandes.edu.co>
2021-11-07 22:43:52 -0500

    se crean dos test para validar las funcionalidades de coleccionista y visitante

--- app/src/androidTest/java/com/project/vinilos/view/VisitanteTest.kt
+++ app/src/androidTest/java/com/project/vinilos/view/VisitanteTest.kt
@@ -3,9 +3,11 @@ package com.project.vinilos.view
 
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
@@ -16,52 +18,135 @@ import org.hamcrest.Matcher
 import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.allOf
 import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
-class MainActivityTest {
+class VisitanteTest  {
 
     @Rule
     @JvmField
     var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
 
     @Test
-    fun mainActivityTest() {
-        val button = onView(
+    fun visitanteTest() {
+        val textView = onView(
             allOf(
-                withId(R.id.collector_button), withText("Coleccionista"),
+                withId(R.id.textView), withText("Vinilos App"),
                 withParent(withParent(withId(R.id.viewRoot))),
                 isDisplayed()
             )
         )
-        button.check(matches(isDisplayed()))
+        textView.check(matches(withText("Vinilos App")))
 
-        val button2 = onView(
+        val imageView = onView(
+            allOf(
+                withId(R.id.dice_image),
+                withParent(withParent(withId(R.id.viewRoot))),
+                isDisplayed()
+            )
+        )
+        imageView.check(matches(isDisplayed()))
+
+        val button = onView(
             allOf(
                 withId(R.id.visitor_button), withText("Visitante"),
                 withParent(withParent(withId(R.id.viewRoot))),
                 isDisplayed()
             )
         )
-        button2.check(matches(isDisplayed()))
+        button.check(matches(isDisplayed()))
 
         val materialButton = onView(
             allOf(
-                withId(R.id.collector_button), withText("Coleccionista"),
+                withId(R.id.visitor_button), withText("Visitante"),
                 childAtPosition(
                     childAtPosition(
                         withId(R.id.viewRoot),
                         0
                     ),
-                    2
+                    3
                 ),
                 isDisplayed()
             )
         )
         materialButton.perform(click())
+
+        val textView2 = onView(
+            allOf(
+                withText("Vinilos App"),
+                withParent(
+                    allOf(
+                        withId(R.id.toolbar),
+                        withParent(IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        textView2.check(matches(withText("Vinilos App")))
+
+        val view = onView(
+            allOf(
+                withId(R.id.toolbar),
+                withParent(withParent(IsInstanceOf.instanceOf(android.widget.FrameLayout::class.java))),
+                isDisplayed()
+            )
+        )
+        view.check(matches(isDisplayed()))
+
+        val imageButton = onView(
+            allOf(
+                withContentDescription("Navigate up"),
+                withParent(
+                    allOf(
+                        withId(R.id.toolbar),
+                        withParent(IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        imageButton.check(matches(isDisplayed()))
+
+        val imageView2 = onView(
+            allOf(
+                withContentDescription("More options"),
+                withParent(withParent(withId(R.id.toolbar))),
+                isDisplayed()
+            )
+        )
+        imageView2.check(matches(isDisplayed()))
+
+        val view2 = onView(
+            allOf(
+                withId(R.id.rvAlbums),
+                withParent(withParent(withId(android.R.id.content))),
+                isDisplayed()
+            )
+        )
+        view2.check(matches(isDisplayed()))
+
+        val textView3 = onView(
+            allOf(
+                withId(R.id.tvAlbumTitle), withText("Albumes"),
+                withParent(withParent(withId(android.R.id.content))),
+                isDisplayed()
+            )
+        )
+        textView3.check(matches(withText("Albumes")))
+
+        val imageView6 = onView(
+            allOf(
+                withContentDescription("More options"),
+                withParent(withParent(withId(R.id.toolbar))),
+                isDisplayed()
+            )
+        )
+        imageView6.check(matches(isDisplayed()))
 
         val appCompatImageButton = onView(
             allOf(
@@ -81,38 +166,7 @@ class MainActivityTest {
         )
         appCompatImageButton.perform(click())
 
-        val materialButton2 = onView(
-            allOf(
-                withId(R.id.visitor_button), withText("Visitante"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(R.id.viewRoot),
-                        0
-                    ),
-                    3
-                ),
-                isDisplayed()
-            )
-        )
-        materialButton2.perform(click())
 
-        val appCompatImageButton2 = onView(
-            allOf(
-                withContentDescription("Navigate up"),
-                childAtPosition(
-                    allOf(
-                        withId(R.id.toolbar),
-                        childAtPosition(
-                            withClassName(`is`("com.google.android.material.appbar.AppBarLayout")),
-                            0
-                        )
-                    ),
-                    1
-                ),
-                isDisplayed()
-            )
-        )
-        appCompatImageButton2.perform(click())
     }
 
     private fun childAtPosition(
