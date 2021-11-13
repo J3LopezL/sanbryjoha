d7a72ffd51ebb5d423e087e605a193f4385ffe89
Harold Funeme <h.funeme@uniandes.edu.co>
2021-11-12 22:38:56 -0500

    add models of collector

---
+++ app/src/main/java/com/project/vinilos/model/data/models/CollectorAlbums.kt
@@ -0,0 +1,9 @@
+package com.project.vinilos.model.data.models
+
+import java.io.Serializable
+
+data class CollectorAlbums (
+    val id:Int,
+    val price:Int,
+    val status:String,
+    ) : Serializable
