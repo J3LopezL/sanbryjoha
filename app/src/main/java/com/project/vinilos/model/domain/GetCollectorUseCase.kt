007aaceb4fe63907506727fc65f273481c9b91a5
Harold Funeme <72938371+hfuneme@users.noreply.github.com>
2021-11-21 15:34:27 -0500

    removing imports unnecessaries GetCollectorUseCase

--- app/src/main/java/com/project/vinilos/model/domain/GetCollectorUseCase.kt
+++ app/src/main/java/com/project/vinilos/model/domain/GetCollectorUseCase.kt
@@ -1,7 +1,6 @@
 package com.project.vinilos.model.domain
 
 import com.project.vinilos.model.data.models.dataClass.Collector
-import com.project.vinilos.model.data.network.CollectorService
 import com.project.vinilos.model.data.repositories.CollectorRepository
 
 class GetCollectorUseCase {
