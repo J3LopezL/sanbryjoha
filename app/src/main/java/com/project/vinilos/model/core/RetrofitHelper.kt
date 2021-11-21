93897e30b9c070139b001b171edf801eef7b56d7
Harold Funeme <72938371+hfuneme@users.noreply.github.com>
2021-11-21 15:21:49 -0500

    removing imports unnecessaries RetrofitHelper

--- app/src/main/java/com/project/vinilos/model/core/RetrofitHelper.kt
+++ app/src/main/java/com/project/vinilos/model/core/RetrofitHelper.kt
@@ -2,9 +2,8 @@ package com.project.vinilos.model.core
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
-// metodo de configuracion de retrofit para hacer la peticion y convertir Json en Obj Dataclass
+
 object RetrofitHelper {
     fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
