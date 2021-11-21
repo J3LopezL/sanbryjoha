3c73d0a4dd5468d314d8254aafb9778c7efd6590
Harold Funeme <72938371+hfuneme@users.noreply.github.com>
2021-11-21 15:37:16 -0500

    removing comments  unnecessaries CollectorViewModel

--- app/src/main/java/com/project/vinilos/viewmodel/CollectorViewModel.kt
+++ app/src/main/java/com/project/vinilos/viewmodel/CollectorViewModel.kt
@@ -9,21 +9,13 @@ import kotlinx.coroutines.launch
 
 class CollectorViewModel: ViewModel() {
 
-    // encapsula el model que tenemos en un LiveData
-    // se mete en un mutable por que el valor va a cambiar
     val collectorModelLiveData = MutableLiveData<List<Collector>>()
     val getCollectorUseCase = GetCollectorUseCase()
 
     fun  onCreate() {
-        println("****************************************")
-        println("CollectorViewModel")
-        // corrutina - Se ejecuta en otro hilo para no bloquear el principal
-        // scope es donde se va a ejecutar la corrutuina
         viewModelScope.launch {
             val result = getCollectorUseCase()
-            // verifica si la respuesta de la api no es null o empty y la ponemos en el liveData para uso en la activity
             if (!result.isNullOrEmpty()){
-                // Se notifica de los cambio que se presentan en el MutableLiveData en este caso en el DataClass Collector
                 collectorModelLiveData.value = result
             }
         }
